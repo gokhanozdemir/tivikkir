79d16e0df20be37db0bc254d24409d77a01cb0a0
Gökhan <gn.ozdemir@gmail.com>
2024-12-05 11:10:27 +0300

    tweetleri listeler

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageLayout from '../PageLayout'
+import Twit from './Twit'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 // https://kiwitter-node-77f5acb427c1.herokuapp.com/twits
 function Home() {
 
 	// Queries
-	const { data, isLoading, error } = useQuery({
+	const { data: twits, isLoading, error, isError } = useQuery({
 		queryKey: ['mainPageTwits'], queryFn: () => {
-			return axios.get('https://kiwittesdfr-node-77f5acb427c1.herokuapp.com/twits')
+			return axios.get('https://kiwitter-node-77f5acb427c1.herokuapp.com/twits')
 		}
 	})
-
 
 	return (
 		<PageLayout>
@@ -20,8 +20,9 @@ function Home() {
 
 			{isLoading && <p>Loading...</p>}
 			{error && <p>{error.message}</p>}
-			{data?.data && JSON.stringify(data.data)}
-			{/* twit listesi */}
+			{!isError && !isLoading && twits.data.data?.map((item) =>
+				<Twit key={item.id} item={item} />
+			)}
 		</PageLayout>
 	)
 }
---
+++ src/components/Twit.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+
+import { ReplyAll, Ellipsis, Heart, MessageCircleMore } from 'lucide-react';
+
+export default function Twit({ item, twitType = "main" }) {
+  return (
+    <div
+      key={item.author_id}
+      className={`px-6 py-7 border-b border-gray-300 last:border-b-0 flex gap-6 items-start ${twitType === "reply" ? "bg-gray-100" : ""
+        }`}
+    >
+      <div className="rounded-full bg-gray-200 text-gray-600 font-black text-2xl text-center p-6 size-20 shrink-0">
+        {item.name.split(" ").map((a) => a[0])}
+      </div>
+      <div>
+        <Link to={`/profile/${item.nickname}`} className="flex gap-2 pt-1">
+          <span className="font-bold">{item.name}</span>
+          <span className="text-gray-600">@{item.nickname}</span>
+        </Link>
+        <p className="mt-1">{item.content}</p>
+        <div className="flex gap-2 mt-2 items-center">
+          <button className="flex gap-1 text-sm items-center bg-gray-100 border border-gray-300 hover:bg-gray-200 transition-all py-1.5 px-2 rounded-lg">
+            <Heart />{" "}
+            {item.likes !== "0" && item.likes}
+          </button>
+          {twitType === "main" && (
+            <>
+              <Link
+                to={`/detail/${item.id}`}
+                className="flex gap-1 text-sm items-center bg-gray-100 border-gray-300 border hover:bg-gray-200 transition-all py-1.5 px-2 rounded-lg"
+              > <ReplyAll />
+                <MessageCircleMore />
+                {item.replies !== "0" && item.replies}
+              </Link>
+              <Link to={`/detail/${item.id}`} className="p-2">
+                <Ellipsis />{" "}
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
